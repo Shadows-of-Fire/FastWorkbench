62ec338b4d14ad069a37c6cc2e8021c1b5a08946
Shadows_of_Fire <Bward7864@gmail.com>
2018-03-28 17:17:44 -0400

    131

--- src/main/java/shadows/fastbench/FastBench.java
+++ src/main/java/shadows/fastbench/FastBench.java
@@ -31,7 +31,7 @@ public class FastBench {
 
 	public static final String MODID = "fastbench";
 	public static final String MODNAME = "FastWorkbench";
-	public static final String VERSION = "1.3.0";
+	public static final String VERSION = "1.3.1";
 
 	public static final Logger LOG = LogManager.getLogger(MODID);
 
