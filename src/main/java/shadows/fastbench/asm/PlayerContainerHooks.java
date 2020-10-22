fe6b14c21f5c6028feabb9c2b184ae15be12a6fd
Shadows_of_Fire <Bward7864@gmail.com>
2020-10-22 19:46:23 -0400

    doc hooks

--- src/main/java/shadows/fastbench/asm/PlayerContainerHooks.java
+++ src/main/java/shadows/fastbench/asm/PlayerContainerHooks.java
@@ -7,10 +7,21 @@ import shadows.fastbench.gui.CraftingInventoryExt;
 
 public class PlayerContainerHooks {
 
+	/**
+	 * ASM Hook: Called from {@link PlayerContainer#onCraftMatrixChanged(net.minecraft.inventory.IInventory)}
+	 * Vanilla code is nulled out, this method is invoked and a return is inserted afterwards.
+	 * @param ctr The player container.
+	 */
 	public static void onCraftMatrixChanged(PlayerContainer ctr) {
 		ContainerFastBench.slotChangedCraftingGrid(ctr.player.world, ctr.player, (CraftingInventoryExt) ctr.craftMatrix, ctr.craftResult);
 	}
 
+	/**
+	 * ASM Hook: Called from {@link PlayerContainer#transferStackInSlot(net.minecraft.entity.player.PlayerEntity, int)}
+	 * Vanilla code is nulled out, this method is invoked and a return is inserted afterwards.
+	 * @param ctr The player container.
+	 * @return Something, the way this method works makes no sense.
+	 */
 	public static ItemStack transferStackInSlot(PlayerContainer ctr) {
 		return ContainerFastBench.handleShiftCraft(ctr.player, ctr, ctr.inventorySlots.get(0), (CraftingInventoryExt) ctr.craftMatrix, ctr.craftResult, 9, 45);
 	}
